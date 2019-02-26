4bf4ecbe6d2d083e6f36465492b084b4d9a941d8
Oliver Schwartz <ollieschwartz99@gmail.com>
2019-02-25 20:45:36 -0500

    updated requirements.txt with crispy-forms

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.models import Users
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 # Inherits from UserCreationForm class
