892c004a55a8e1e41df46f4c697651e82907d716
Oliver Schwartz <ollieschwartz99@gmail.com>
2019-03-11 11:56:09 -0400

    starting afresh

--- mysite/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # maps to home function in views.py
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
-]
