f8c78b98378c1b4ecaa8c955c7c3624c5ee954ba
Oliver Schwartz <ollieschwartz99@gmail.com>
2019-03-13 14:47:33 -0400

    map icons working, custom dark map, zoom tools enabled

--- pickup/maps/js/map.js
+++
@@ -1,7 +0,0 @@
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
-mapboxgl.accessToken = '{{ mapbox_access_token }}';
-
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9'
-});
