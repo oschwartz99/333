c89df4de68614ca24a3334fbca410096d3722407
Oliver Schwartz <ollieschwartz99@gmail.com>
2019-03-14 16:16:03 -0400

    problems importing package in pickup/maps/static/js/load.js

--- pickup/maps/static/js/load.js
+++ pickup/maps/static/js/load.js
@@ -1,15 +1,15 @@
-// import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
+import mapboxgl from './node_modules/mapbox-gl';
 
-// mapboxgl.accessToken = 'pk.eyJ1IjoiY29zMzMzIiwiYSI6ImNqdDYzY3A0ZDBkMGc0YXF4azczdXRheWMifQ.3VeYeV_c-231Lab62H2XtQ';
-// const map = new mapboxgl.Map({
-//     container: 'map',
-//     style: 'mapbox://styles/mapbox/streets-v9',
-//     center: [151.21, -33.86],
-//     zoom: 8,
-// });
+mapboxgl.accessToken = 'pk.eyJ1IjoiY29zMzMzIiwiYSI6ImNqdDYzY3A0ZDBkMGc0YXF4azczdXRheWMifQ.3VeYeV_c-231Lab62H2XtQ';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [151.21, -33.86],
+    zoom: 8,
+});
 
-// map.on('load', () => {
-//     map.addControl(new mapboxgl.NavigationControl());
-// });
+map.on('load', () => {
+    map.addControl(new mapboxgl.NavigationControl());
+});
 
 alert('hello');
