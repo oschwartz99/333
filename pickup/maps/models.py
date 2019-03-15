d6bdbfc46cb61c64a9c582ebb35a296277afaedf
Oliver Schwartz <ollieschwartz99@gmail.com>
2019-03-15 11:08:41 -0400

    events changes

--- pickup/maps/models.py
+++ pickup/maps/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Event(models.Model):
-    event_name = models.CharField(max_length = 50)
-    event_type = models.CharField(max_length=50)
+    event_name   = models.CharField(max_length = 50)
+    event_type   = models.CharField(max_length=50)
     number_going = models.IntegerField()
+    location     = models.CharField(max_length = 50)
+    icon         = models.CharField(max_length = 50)
+
