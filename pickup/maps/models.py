2e2ebfb93c0d9d8041e5fd5c677e1fced77b0bd0
Oliver Schwartz <ollieschwartz99@gmail.com>
2019-03-15 11:36:41 -0400

    creating models

--- pickup/maps/models.py
+++ pickup/maps/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+
 
 class Event(models.Model):
+    user         = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    event_descr  = models.CharField(max_length = 100)
     event_name   = models.CharField(max_length = 50)
     event_type   = models.CharField(max_length=50)
     number_going = models.IntegerField()
