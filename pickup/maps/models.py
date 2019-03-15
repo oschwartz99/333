c3d2c8d3a85d04d8602582de9b157ca1b67be1f9
Oliver Schwartz <ollieschwartz99@gmail.com>
2019-03-15 11:52:08 -0400

    created testing page

--- pickup/maps/models.py
+++ pickup/maps/models.py
@@ -4,11 +4,11 @@ from django.conf import settings
 
 
 class Event(models.Model):
-    user         = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-    event_descr  = models.CharField(max_length = 100)
-    event_name   = models.CharField(max_length = 50)
-    event_type   = models.CharField(max_length=50)
-    number_going = models.IntegerField()
-    location     = models.CharField(max_length = 50)
-    icon         = models.CharField(max_length = 50)
+    user         = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, default=0)
+    event_descr  = models.CharField(max_length = 100, default='descr')
+    event_name   = models.CharField(max_length = 50, default='')
+    event_type   = models.CharField(max_length=50, default='')
+    number_going = models.IntegerField(default=0)
+    location     = models.CharField(max_length = 50, default='')
+    icon         = models.CharField(max_length = 50, default='')
 
