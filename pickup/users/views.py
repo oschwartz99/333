69f6052da60aa211774d336827393eaaea715282
David Zuluaga <davidzuluaga@yahoo.com>
2019-03-18 18:39:25 -0400

    Create a standard_page template with side bar all other pages can extent & a base profile page

--- pickup/users/views.py
+++ pickup/users/views.py
@@ -1,6 +1,7 @@
 # users/views.py
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +9,7 @@ class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+def profile_page(request):
+    return render(request, 'profile_page.html')
