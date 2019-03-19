1dcb733f950c7f4f92dcfa6d227addf35605fd4d
alexb5167 <47536519+alexb5167@users.noreply.github.com>
2019-03-19 13:40:55 -0400

    Add files via upload
    Testing change on profile page

---
+++ 333/pickup/users/urls.py
@@ -0,0 +1,9 @@
+# users/urls.py
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+urlpatterns = [
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    url('profile/', views.profile_page, name='profile_page')
+]
