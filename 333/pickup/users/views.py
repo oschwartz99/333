f216ca938a259c6ef2a55d9d92b4e5e23a887756
alexb5167 <47536519+alexb5167@users.noreply.github.com>
2019-03-19 15:56:33 -0400

    Add files via upload
    Added basic version of the feature allowing you to view and edit your own profile

--- 333/pickup/users/views.py
+++ 333/pickup/users/views.py
@@ -1,8 +1,9 @@
 # users/views.py
 from django.urls import reverse_lazy
 from django.views import generic
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models  import User
+from django.contrib.auth.forms import UserChangeForm
 from .forms import CustomUserCreationForm
 
 class SignUp(generic.CreateView):
@@ -10,6 +11,17 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
-
 def profile_page(request):
+    args = {'user': request.user}
     return render(request, 'profile_page.html')
+
+def profile_edit(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('/users/profile/')
+    else:
+        form = UserChangeForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'profile_edit.html', args)
