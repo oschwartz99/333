6b28781aed9efcd68ff892f994515909d159956b
Oliver Schwartz <ollieschwartz99@gmail.com>
2019-03-19 20:01:57 -0400

    removed duplicate files

--- 333/pickup/maps/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class CreateEvent(forms.Form):
-    event_descr  = forms.CharField(max_length=100)
-    event_name   = forms.CharField(max_length = 50)
-    event_type   = forms.CharField(max_length=50)
-    number_going = forms.IntegerField()
-    location     = forms.CharField(max_length = 50)
--- 333/pickup/maps/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url                                                                                                                              
-from . import views
-
-urlpatterns = [ 
-    url('testing/list_events/', views.testing_list_events, name = "list_events"),
-    url('testing/map_def', views.testing_map_def, name='map_def'),
-    url('testing/', views.testing_view, name="testing"),
-    url('', views.default_map, name="maps"),
-]
