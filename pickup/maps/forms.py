5a28c26830c308c345f1646eff9ff1909dabc4a3
Oliver Schwartz <ollieschwartz99@gmail.com>
2019-03-23 17:46:55 -0400

    created dropdown menu for event_type

--- pickup/maps/forms.py
+++ pickup/maps/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 
+EVENT_CHOICES = (
+    ("Party", "Party"),
+    ("Concert", "Concert"),
+)
+
 class CreateEvent(forms.Form):
     event_descr  = forms.CharField(max_length=100)
     event_name   = forms.CharField(max_length = 50)
-    event_type   = forms.CharField(max_length=50)
+    event_type   = forms.ChoiceField(choices=EVENT_CHOICES, required=True)
     number_going = forms.IntegerField()
     location     = forms.CharField(max_length = 50)
     lng          = forms.FloatField()
     lat          = forms.FloatField()
+
--- pickup/maps/models.py
+++ pickup/maps/models.py
@@ -1,19 +1,23 @@
 from django.db import models
+from django import forms
 from django.contrib.auth.models import User
 from django.conf import settings
+
+EVENT_CHOICES = (
+    ("Party", "Party"),
+    ("Concert", "Concert"),
+)
 
 class Event(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.PROTECT, default=-1)
     event_descr = models.CharField(max_length=100, default='')
     event_name = models.CharField(max_length=50, default='')
-    event_type = models.CharField(max_length=50, default='')
+    event_type = models.CharField(max_length=50, choices=EVENT_CHOICES)
     number_going = models.IntegerField(default=-1)
     location = models.CharField(max_length=50, default='')
     lng = models.FloatField(default=-1)
     lat = models.FloatField(default=-1)
-
-
 
 class DebugEvent(models.Model):
     user = models.CharField(max_length=50, default='')
