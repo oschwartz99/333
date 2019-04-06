2f2fff9c1693d952b479f830a250192cb223beb2
Gabriel Stengel (Macbook Pro) <gstengel@princeton.edu>
2019-04-06 16:06:11 -0400

    script edit, nothing update

--- pickup/setuppickup.sh
+++ pickup/setuppickup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 rm db.sqlite3
+rm maps/migrations/00*
+rm users/migrations/00*
 python3 manage.py makemigrations
-python3 manage.py migrate
+python manage.py migrate --run-syncdb
 python3 manage.py createsuperuser
 python3 manage.py runserver
