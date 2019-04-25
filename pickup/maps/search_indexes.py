7ae558f1ccc4317a555c545ab0045c9b38012538
David Zuluaga <davidzuluaga@yahoo.com>
2019-04-25 14:37:12 -0400

    clean file

--- pickup/maps/search_indexes.py
+++ pickup/maps/search_indexes.py
@@ -3,17 +3,17 @@ from .models import Event
 
 
 class EventIndex(indexes.SearchIndex, indexes.Indexable):
-	text = 			indexes.EdgeNgramField(document=True, use_template=True)
-	event_name = 	indexes.CharField(model_attr='event_name')
-	event_descr = 	indexes.CharField(model_attr='event_descr')
-	location = 		indexes.CharField(model_attr='location')
-	created_by =	indexes.CharField(model_attr='user')
-	event_type = 	indexes.CharField(model_attr='event_type')
-	public = 		indexes.BooleanField(model_attr='public')
-	lng = 			indexes.FloatField(model_attr='lng')
-	lat = 			indexes.FloatField(model_attr='lat')
-	id = 			indexes.CharField(model_attr='pk')
-	date = 			indexes.DateField(model_attr='date')
+	text 			= indexes.EdgeNgramField(document=True, use_template=True)
+	event_name 		= indexes.CharField(model_attr='event_name')
+	event_descr 	= indexes.CharField(model_attr='event_descr')
+	location 		= indexes.CharField(model_attr='location')
+	created_by 		= indexes.CharField(model_attr='user')
+	event_type 		= indexes.CharField(model_attr='event_type')
+	public 			= indexes.BooleanField(model_attr='public')
+	lng 			= indexes.FloatField(model_attr='lng')
+	lat 			= indexes.FloatField(model_attr='lat')
+	id 				= indexes.CharField(model_attr='pk')
+	date 			= indexes.DateField(model_attr='date')
 
 	def get_model(self):
 		return Event
