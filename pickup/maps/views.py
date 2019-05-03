27da3525b3df69851a403b6ca921bfdfd7acc84e
David Zuluaga <davidzuluaga@yahoo.com>
2019-05-02 20:51:26 -0400

    ux changes

--- pickup/maps/views.py
+++ pickup/maps/views.py
@@ -81,15 +81,18 @@ def event_search(request):
         events = None
     else:
         # return all queries that match the search
-        event_search_all = Event.objects.all()
+        event_search_all = list(SearchQuerySet().models(Event).autocomplete(text=search_text))
         friends = Friend.objects.friends(request.user)
         events = []
         for event in event_search_all:
             # do not show event if it private and not created by the user or their friends
             if event.public or event.created_by == request.user or event.created_by in friends:
-                if datetime.now().date() <= event.date:
-                    print(event.event_name)
-                    events.append(event)
+                if timezone.is_aware(event.date):
+                    if timezone.now() <= event.date:
+                        events.append(event)
+                else:
+                    if datetime.now() <= event.date:
+                        events.append(event)
 
     return render_to_response('ajax-search.html', {'events': events})
 
